004be866fdf057b5f869dc068b0be5e17c5e4110
sauravstr <60398044+sauravstr@users.noreply.github.com>
2022-10-22 12:57:07 -0500

    magic8.py
    Optional Challenges Added!

--- 2-control-flow/magic-8-ball/magic8.py
+++ 2-control-flow/magic-8-ball/magic8.py
@@ -1,32 +1,44 @@
 import random
 
-name = "Joe"
-question = "Will I win the lottery?"
+name = "Sam"
+question = "Will it rain today?"
 answer = ""
 
-random_number = random.randint(1, 9)
+random_number = random.randint(1, 10)
 # print(random_number)
 
 if random_number == 1:
-  answer = "Yes - definitely"
+  answer = "Yes - definitely."
 elif random_number == 2:
-  answer = "It is decidedly so"
+  answer = "It is decidedly so."
 elif random_number == 3:
-  answer = "Without a doubt"
+  answer = "Without a doubt."
 elif random_number == 4:
-  answer = "Reply hazy, try again"
+  answer = "Reply hazy, try again."
 elif random_number == 5:
-  answer = "Ask again later"
+  answer = "Ask again later."
 elif random_number == 6:
-  answer = "Better not tell you now"
+  answer = "Better not tell you now."
 elif random_number == 7:
-  answer = "My sources say no"
+  answer = "My sources say no."
 elif random_number == 8:
-  answer = "Outlook not so good"
+  answer = "Outlook not so good."
 elif random_number == 9:
-  answer = "Very doubtful"
+  answer = "Very doubtful."
+elif random_number == 10:   # Additional Random Answer
+  answer = "Check weather.com"
 else:
   answer = "Error"
-  
-print(name + " asks: " + question)
-print("Magic 8 Ball's answer: " + answer)
+
+# Optional Challenges
+if name != "":
+  print(name + " asks: " + question)
+else:
+  print("Question: ", question)
+
+if question == "":
+  print("Magic 8-Ball expects some question!")
+else:
+  print("Magic 8-Ball's answer: ", answer)
+
+
